3d201235ba6cc5fa7cf04ad04fa8a8d129d1b455
Roshan Rino <roshanrino.me19@bitsathy.ac.in>
2022-04-12 19:01:35 +0530

    Explicit destructor with no arguments

---
+++ Destructors/1. Destructors.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Person
+{
+    int id;
+    string name;
+public:
+    Person(int a,string b)
+    {
+        id=a;
+        name=a;
+    }
+    ~Person()
+    {
+        cout<<"Person Destroyed"<<endl;
+    }
+};
+int main()
+{
+    Person p(1203,"Bob");
+
+}
